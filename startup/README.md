b6bfbd7e4ffe9100a8a18014cba440107fae57d6
shaikabduladil <adil30205@gmail.com>
2024-07-10 22:51:55 +0530

    created react with HTML nd js

---
+++ startup/README.md
@@ -0,0 +1,1 @@
+#hello this is react course from Namaste React
---
+++ startup/app.js
@@ -0,0 +1,32 @@
+// //creating single element
+// const heading = React.createElement(
+//   "h1",
+//   {
+//     id: "heading",
+//     className: "myHeading",
+//   },
+//   "this is Adil"
+// );
+// console.log(heading); //heading is an object
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading); //render method reponsible for rendering heading object into html
+
+//creating multiple elements
+//for creating multiple elements or siblings we need to use 3rd parameter as array of elements
+
+const parent = [
+  React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child" }, [
+      React.createElement("h1", {}, "this is H1"),
+      React.createElement("h2", {}, "this is h2"),
+    ]),
+    React.createElement("div", { id: "child2" }, [
+        React.createElement("h1", {}, "this is H1"),
+        React.createElement("h2", {}, "this is h2"),
+      ]),
+  ]),
+];
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
